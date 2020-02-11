de2993cb2f5e6dac87ae0db3f11e9b2842feb612
PoojaBayas <pooja.bayas32@gmail.com>
2020-02-11 13:53:52 -0800

    Chnages package-json file, and tested html appComponent.html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TvShowComponent } from './tv-show/tv-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvShowComponent
   ],
   imports: [
     BrowserModule
